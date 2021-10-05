f84d30d69e6f8d53c5c649bbb1c2cf91a9fb70db
Geoffrey Auzou <geoffrey.auzou@etu.univ-orleans.fr>
2021-10-05 14:11:39 +0200

    Add src + connexion bd + model plat

---
+++ src/delivecrous.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/delivecrous");
+
+const Plat = mongoose.model("Plat", {
+    nom : String,
+    description : String,
+    prix : float,
+    allergene : string
+})
+
+app.get("*", (req, res) => {
+    res.status(404).end()
+    console.log("error 404")
+})
+
+app.listen(8000);
