0a67d48512f70b553a5eaf36690825a2e7062b28
Geoffrey Auzou <geoffrey.auzou@etu.univ-orleans.fr>
2021-10-05 17:06:45 +0200

    Add GET cart/id show plats

--- src/delivecrous.js
+++ src/delivecrous.js
@@ -9,35 +9,42 @@ app.use(bodyParser.json());
 
 mongoose.connect("mongodb://localhost:27017/delivecrous");
 
-// afficher tous les plats
+// Afficher tous les plats
 app.get("/dishes", async (req,res) => {
     Plat.find()
         .then((plats) => res.json(plats))
         .catch(() => res.status(404).end())
 })
 
-// afficher un plat par ID
-app.get("/plats/:id", async (req, res) => {
+// Afficher un plat par ID
+app.get("/dishes/:id", async (req, res) => {
     Plat.findById(req.params.id)
         .then((plat) => res.json(plat))
         .catch(() => res.status(404).end())
 })
 
-// mise à jour du panier
+// Mise à jour du panier
 app.put("/cart", async (req,res) => {
     Panier.findByIdAndUpdate(req.params.id, req.body)
         .then((panier) => res.json(panier))
         .catch(() => res.status(404).end())
 })
 
-// suppression du panier
+// Suppression du panier
 app.delete("/cart/:id", async (req,res) => {
     Panier.findByIdAndDelete(req.params.id)
         .then((panier) => res.json(panier))
         .catch(() => res.status(404).end())
 })
 
-// route par défaut
+// Afficher liste article du panier
+app.get("/cart/:id", async (req, res) => {
+    Panier.findById(req.params.id)
+        .then((panier) => res.json(panier["plats"]))
+        .catch(() => res.status(404).end())
+})
+
+// Route par défaut
 app.get("*", (req, res) => {
     res.status(404).end()
     console.log("error 404")
