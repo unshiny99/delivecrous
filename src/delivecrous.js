6957609e13e24b77ec72df3eeae684de52ed0308
JoJo-fr <geoffrey.auzou@gmail.com>
2021-12-22 23:06:36 +0100

    Modification verifyToken(correction) + ajout vérification token (all fonction) + login vérification client(Maxime)

--- src/delivecrous.js
+++ src/delivecrous.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const jwt = require('jsonwebtoken');
 
-const {Panier, Plat} = require('./models/panier');
 const Client = require('./models/client');
+const {Panier, Plat} = require('./models/panier');
 const { callbackify } = require("util");
+
 const res = require("express/lib/response");
+const { nextTick } = require("process");
 
 TOKEN_SECRET = require('crypto').randomBytes(64).toString('hex');
 
@@ -17,42 +19,57 @@ mongoose.connect("mongodb://localhost:27017/delivecrous");
 
 // Génération du token
 function generateAccessToken(username) {
-    return jwt.sign(username, 
-                    TOKEN_SECRET, 
-                    { expiresIn: '60s'});
+    return jwt.sign(
+        username, 
+        TOKEN_SECRET, 
+        { expiresIn: '60s'}
+    );
 }
 
 // Vérification de la validité du token
-function verifyToken(username, token){
-    return jwt.verify(token, TOKEN_SECRET, (err, decoded) => { 
+function verifyToken(req, res, next){
+    return jwt.verify(req.headers['authorization'],
+                        TOKEN_SECRET, (err, decoded) => { 
         if(err){
-            return {'status': err['message']}
+            res.send(JSON.stringify({
+                error : err
+            }))
         }else{
-            return {'status': "accepte"};
+            next()
         }
      });
 }
 
-// Fonction de test : Verify token
+// Fonction de test : Verify token (DEBUG)
 app.post("/test", async (req, res) => {
-    res.json(verifyToken(req.body.username, req.body.token));
+    res.json(verifyToken(req));
 })
 
 // Login et génération du token
 app.post("/login", async (req, res) => {
-    const token = generateAccessToken({username: req.body.username});
-    res.json(token);
+    // vérification si le client existe puis génération token
+    Client.findOne({username : req.body.username}, function(err, client) {
+        if(err) throw res.status(404).end();
+        if(client) {
+            const token = generateAccessToken({username: req.body.username});
+            res.json(token);
+        } else {
+            res.send(JSON.stringify({
+                error : "le client n'existe pas dans la data base"
+            }))
+        }
+    });
 })
 
 // Afficher tous les plats
-app.get("/dishes", async (req, res) => {
+app.get("/dishes", verifyToken, async (req, res) => {
     Plat.find()
         .then(plats => res.status(200).json(plats))
         .catch(() => res.status(404).end())
 })
 
 // Afficher un plat par ID
-app.get("/dish/:id", async (req, res) => {
+app.get("/dish/:id", verifyToken, async (req, res) => {
     Plat.findOne({id_plat : req.params.id },
         function(err, plat){
             if (err) throw res.status(404).end();
@@ -67,8 +84,24 @@ app.get("/dish/:id", async (req, res) => {
     )
 })
 
+// afficher un plat par son nom (fixture challenge)
+app.get("/dish/:nom", verifyToken, async (req, res) => {
+    Plat.findOne({nom : req.params.nom },
+        function(err, plat){
+            if (err) throw res.status(404).end();
+            if (plat) {
+                // res.status(200).json(plat);
+            } else {
+                res.send(JSON.stringify({
+                    error : "le plat n'existe pas dans la data base"
+                }))
+            }
+        }
+    )
+})
+
 // ajout d'un article au panier
-app.post("/cart/:id", async (req,res) => {
+app.post("/cart/:id", verifyToken, async (req,res) => {
     const panier = await Panier.findOne({id_panier : 1});
     Plat.findOne({id_plat : req.params.id}, 
         function(err, plat){
@@ -88,7 +121,7 @@ app.post("/cart/:id", async (req,res) => {
 })
 
 // Suppression d'un article du panier
-app.delete("/cart/:id", async (req,res) => {
+app.delete("/cart/:id", verifyToken, async (req,res) => {
     const panier = await Panier.findOne({id_panier : 1});
     Plat.findOne({id_plat : req.params.id}, 
         function(err, plat){
@@ -107,18 +140,18 @@ app.delete("/cart/:id", async (req,res) => {
 })
 
 // Afficher liste article(s) du panier
-app.get("/cart", async (req, res) => {
+app.get("/cart", verifyToken, async (req, res) => {
     Panier.find()
         .then((panier) => res.status(200).json(panier))
         .catch(() => res.status(404).end())
 })
 
 // Gérer l'adresse du client
-app.put("/cart_validation", async (req, res) => {
+app.put("/cart_validation", verifyToken, async (req, res) => {
     Panier.findOne({id_panier : 1}, 
         function(err, panier){
             if(err) throw res.status(404).end();
-            if(panier && req.body.rue != "" && req.body.code_postal != "" && req.body.ville) {
+            if(panier && req.body.rue != "" && req.body.code_postal != "" && req.body.ville != "") {
                 Panier.updateOne(panier, {
                 "rue": req.body.rue,
                 "code_postal": req.body.code_postal,
@@ -136,7 +169,7 @@ app.put("/cart_validation", async (req, res) => {
 })
 
 // Route par défaut
-app.get("*", (req, res) => {
+app.get("*", verifyToken, (req, res) => {
     res.status(404).end()
 })
 
